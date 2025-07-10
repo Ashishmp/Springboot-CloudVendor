2a932caf121735a55bde76f47d6346daf4702100
Ashishmp <ar9034@srmist.edu.in>
2025-07-10 20:23:25 +0530

    added config with DB

---
+++ src/main/java/com/example/demo/coontroller/CloudVendorController.java
@@ -0,0 +1,48 @@
+package com.example.demo.coontroller;
+
+import com.example.demo.model.CloudVendor;
+import com.example.demo.service.CloudVendorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorController {
+    public CloudVendorController(CloudVendorService cloudVendorService) {
+        this.cloudVendorService = cloudVendorService;
+    }
+
+    CloudVendorService cloudVendorService;
+//Read Specific Vendor
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(@PathVariable("vendorId") String vendorId){
+        return cloudVendorService.getCloudVendor(vendorId);
+//                CloudVendor("c1","vendor1","Address one","765437");
+    }
+    // read all vendor details
+    @GetMapping()
+    public List<CloudVendor> getAllCloudVendorDetails(){
+        return cloudVendorService.getAllCloudVendors();
+//                CloudVendor("c1","vendor1","Address one","765437");
+    }
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        cloudVendorService.createCloudVendor(cloudVendor);
+        return "Cloud Vendor Created";
+    }
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        cloudVendorService.updateCloudVendor(cloudVendor);
+        return "Cloud Vendor Updated";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(@PathVariable("vendorId") String vendorId)
+    {
+        cloudVendorService.deleteCloudVendor(vendorId);
+        return "Cloud Vendor deleted";
+    }
+}
---
+++ src/main/java/com/example/demo/service/impl/CloudVendorServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.CloudVendor;
+import com.example.demo.repository.CloudVendorRepository;
+import com.example.demo.service.CloudVendorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class CloudVendorServiceImpl implements CloudVendorService {
+
+    CloudVendorRepository cloudVendorRepository;
+    public CloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository) {
+        this.cloudVendorRepository = cloudVendorRepository;
+    }
+
+
+    @Override
+    public String createCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String updateCloudVendor(CloudVendor cloudVendor) {
+        //add more business logic  in every function
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String deleteCloudVendor(String cloudVendorId) {
+        cloudVendorRepository.deleteById(cloudVendorId);
+        return "Success";
+    }
+
+    @Override
+    public CloudVendor getCloudVendor(String cloudVendorId) {
+        return cloudVendorRepository.findById(cloudVendorId).get();
+
+    }
+
+    @Override
+    public List<CloudVendor> getAllCloudVendors() {
+        return cloudVendorRepository.findAll();
+    }
+}
