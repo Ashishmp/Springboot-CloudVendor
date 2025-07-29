1223e37a7a03f60c64425ef088b0836a35a9fb78
Ashishmp <ar9034@srmist.edu.in>
2025-07-29 15:51:37 +0530

    Added springDOC for API Documenatation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.swing.plaf.basic.BasicToolBarUI;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +14,12 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+@Bean
+	public OpenAPI customOpenAPI(){
+		return  new OpenAPI()
+				.info(new Info()
+						.title("Cloud Vendor API Application")
+						.description("Cloud Vendor API Documentation")
+						.version("1.0"));
+}
 }
