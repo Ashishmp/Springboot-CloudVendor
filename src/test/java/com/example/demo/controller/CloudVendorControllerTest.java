cfc8921348565c83cd7d1dc01621943407761dde
Ashishmp <ar9034@srmist.edu.in>
2025-07-29 14:41:13 +0530

    added test cases config/setting

---
+++ src/test/java/com/example/demo/controller/CloudVendorControllerTest.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CloudVendorControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getCloudVendorDetails() {
+    }
+
+    @Test
+    void getAllCloudVendorDetails() {
+    }
+
+    @Test
+    void createCloudVendorDetails() {
+    }
+
+    @Test
+    void updateCloudVendorDetails() {
+    }
+
+    @Test
+    void deleteCloudVendorDetails() {
+    }
+}
