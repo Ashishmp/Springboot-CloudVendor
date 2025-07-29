6fa4e5ea02279e225b097b52d1a5fe6222019448
Ashishmp <ar9034@srmist.edu.in>
2025-07-29 14:37:55 +0530

    added test cases config/setting

---
+++ src/test/java/com/example/demo/service/impl/CloudVendorServiceImplTest.java
@@ -0,0 +1,54 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.CloudVendor;
+import com.example.demo.repository.CloudVendorRepository;
+import com.example.demo.service.CloudVendorService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class CloudVendorServiceImplTest {
+
+    @Mock
+    private CloudVendorRepository cloudVendorRepository;
+    private CloudVendorService cloudVendorService;
+    AutoCloseable autoCloseable;
+    CloudVendor cloudVendor;
+
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        cloudVendorService = new CloudVendorServiceImpl(cloudVendorRepository);
+        cloudVendor =  new CloudVendor("1", "Amazon", "USA", "XXX");
+
+
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    void createCloudVendor() {
+    }
+
+    @Test
+    void updateCloudVendor() {
+    }
+
+    @Test
+    void deleteCloudVendor() {
+    }
+
+    @Test
+    void getCloudVendor() {
+    }
+
+    @Test
+    void getAllCloudVendors() {
+    }
+}
