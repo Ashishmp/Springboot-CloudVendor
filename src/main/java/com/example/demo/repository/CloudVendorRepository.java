d22b53f035a4eb5879b37198888b8a11aa7eb6ef
Ashishmp <ar9034@srmist.edu.in>
2025-07-29 14:28:13 +0530

    added test cases config/setting

--- src/main/java/com/example/demo/repository/CloudVendorRepository.java
+++ src/main/java/com/example/demo/repository/CloudVendorRepository.java
@@ -3,8 +3,11 @@ package com.example.demo.repository;
 import com.example.demo.model.CloudVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CloudVendorRepository extends JpaRepository <CloudVendor, String> {
     //write custom method if not in JPA
+    List<CloudVendor> findByVendorName(String vendorName);
 
 }
 
